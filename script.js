935d335b9834ab35a09be51efa71243438360fb6
Sumeet <sumitkumarsahoo2930@gmail.com>
2025-12-26 19:59:33 +0530

    Added new paragraph in feature-update branch

---
+++ script.js
@@ -0,0 +1,6 @@
+function checkStatus() {
+    const statusText = document.getElementById("statusText");
+
+    statusText.textContent = "Project is active and Git workflow implemented successfully.";
+    statusText.style.color = "green";
+}
