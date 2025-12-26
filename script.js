4b5db4898e3552037f81e243576f793a37c99f53
Sumeet <sumitkumarsahoo2930@gmail.com>
2025-12-26 20:04:25 +0530

    Updated heading in conflict-branch

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function checkStatus() {
-    const statusText = document.getElementById("statusText");
-
-    statusText.textContent = "Project is active and Git workflow implemented successfully.";
-    statusText.style.color = "green";
+function animateCounter(id, target) {
+    let count = 0;
+    const element = document.getElementById(id);
+    const interval = setInterval(() => {
+        count++;
+        element.textContent = count;
+        if (count === target) clearInterval(interval);
+    }, 40);
 }
+
+window.onload = () => {
+    animateCounter("commits", 28);
+    animateCounter("branches", 6);
+    animateCounter("merges", 12);
+    animateCounter("conflicts", 4);
+};
